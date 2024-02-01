c5728bf3819aaf91ae9839255f0430b215300240
Dhanarooban1 <dhanazx1@gmail.com>
2024-02-01 10:45:48 +0530

    done

--- server.js
+++ server.js
@@ -1,18 +1,11 @@
 const express = require('express');
 const app = express();
-const cors = require('cors');
-const port = 3000;
-
-
-// server.js
-const express = require('express');
-const app = express();
-const port = 3000;
+const port = 4000;
 
 app.get('/ping', (req, res) => {
-  res.send('Pong!');
+  res.send('<h1>Pong!</h1>');
 });
 
 app.listen(port, () => {
-  console.log(`Server is running on port ${port}`);
+        console.log(`Server is running on port ${port}`);
 });
