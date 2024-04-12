bfbf123b5f60c085d3732e4195b4e8acd8e913b1
Dhanarooban1 <dhanazx1@gmail.com>
2024-04-12 12:42:02 +0530

    dpoone

--- Frontend/src/config.js
+++ Frontend/src/config.js
@@ -1,4 +1,4 @@
-// const BASE_URL = 'https://investor-thoughts.onrender.com'
-const BASE_URL = 'http://localhost:5000'
+const BASE_URL = 'https://investor-thoughts.onrender.com'
+// const BASE_URL = 'http://localhost:5000'
 
 export default BASE_URL;
